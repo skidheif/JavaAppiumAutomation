ab0af3f6ace8c673492114e6e2bba6857eab040f
Andrey <skidheif@gmail.com>
2018-07-31 10:41:29 +0300

    Ex5: Тест: сохранение двух статей - complete

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -468,6 +468,190 @@ public class FirstTest {
         );
     }
 
+
+    //Under this line are located homework "Ex5: Тест: сохранение двух статей"
+    @Test
+    public void Ex5SaveTwoArticleToMyListAndDelete()
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find Search Wikipedia input",
+                15
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Swift",
+                "Cannot find search input",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_container'][@index = " + 0 + "]"),
+                "Cannot find the first element in list, searching by Swift",
+                15
+        );
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc = 'More options']"),
+                "Cannot find button to open article options",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text = 'Add to reading list']"),
+                "Cannot find option to add article to reading list",
+                15
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/onboarding_button"),
+                "Cannot find 'Got it' tip overlay",
+                15
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/text_input"),
+                "Cannot find input to set name of art",
+                15
+        );
+
+        String name_of_folder = "Homework Ex5";
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/text_input"),
+                name_of_folder,
+                "Cannot put text into articles folder input",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text = 'OK']"),
+                "Cannot press OK button",
+                15
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/menu_page_search"),
+                "Cannot find search_button",
+                15
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/text1"),
+                "Cannot find the first element of recent search list",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_container'][@index = " + 1 + "]"),
+                "Cannot find the first element in list, searching by Swift",
+                15
+        );
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc = 'More options']"),
+                "Cannot find button to open article options",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text = 'Add to reading list']"),
+                "Cannot find option to add article to reading list",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text = '" + name_of_folder + "']"),
+                "Cannot find folder to add article to reading list",
+                15
+        );
+
+        String x_link = "//android.widget.ImageButton[@content-desc = 'Navigate up']";
+        waitForElementPresent(
+                By.xpath(x_link),
+                "Cannot find X link",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath(x_link),
+                "Cannot close article, cannot find X link",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc = 'My lists']"),
+                "Cannot find navigation button to my lists",
+                15
+        );
+
+        waitForElementPresent(
+                By.xpath("//*[@text = '" + name_of_folder + "']"),
+                "Cannot find created folder",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text = '" + name_of_folder + "']"),
+                "Cannot find created folder",
+                15
+        );
+
+        String text_article = "//*[@text = 'Swift (programming language)']";
+        swipeElementToLeft(
+                By.xpath(text_article),
+                "Cannot find save article"
+        );
+
+        waitForElementNotPresent(
+                By.xpath(text_article),
+                "Cannot delete save article",
+                15
+        );
+
+        String title_path = "//*[@resource-id = 'org.wikipedia:id/page_list_item_container']//*[@text = 'Swift']";
+        waitForElementPresent(
+                By.xpath(title_path),
+                "Cannot find the article with title Swift",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath(title_path),
+                "Cannot find the element with article Swift in the list",
+                15
+        );
+
+
+        WebElement title_element =  waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                15
+        );
+
+        String article_title = title_element.getAttribute("text");
+        Assert.assertEquals(
+                "We see unexpected title",
+                "Swift",
+                article_title
+        );
+    }
+    // End of the homework "Ex5: Тест: сохранение двух статей"
+
+
+
     //under this line are located methods
 
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
@@ -531,7 +715,7 @@ public class FirstTest {
 
     protected void swipeUpQuick()
     {
-        swipeUp(200);
+        swipeUp(300);
     }
 
     protected void swipeUpToFindElement(By by, String error_message, int max_swipes)
@@ -561,7 +745,7 @@ public class FirstTest {
        TouchAction action = new TouchAction(driver);
        action
                .press(right_x,middle_y)
-               .waitAction(300)
+               .waitAction(700)
                .moveTo(left_x, middle_y)
                .release()
                .perform();
